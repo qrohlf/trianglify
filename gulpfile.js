df9f102e6763eb0411bef999797c75df97de8a58
Quinn Rohlf <qr@qrohlf.com>
2014-10-07 17:08:53 -0700

    add version bumping to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var rename = require('gulp-rename');
 var stylish = require('jshint-stylish');
 var del = require('del');
 var mocha = require('gulp-mocha');
+var git = require('gulp-git');
+var bump = require('gulp-bump');
+var filter = require('gulp-filter');
+var tag_version = require('gulp-tag-version');
 
 gulp.task('clean', function(callback) {
   del('trianglify.min.js', callback);
@@ -31,5 +35,25 @@ gulp.task('minify', ['clean', 'jshint', 'test'], function() {
     .pipe(rename('trianglify.min.js'))
     .pipe(gulp.dest('.'));
 });
+
+function version_bump(type) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+        // bump the version number in those files
+        .pipe(bump({type: type}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        .pipe(git.commit('bump package version'))
+
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return version_bump('patch'); });
+gulp.task('feature', function() { return version_bump('minor'); });
+gulp.task('release', function() { return version_bump('major'); });
 
 gulp.task('default', ['minify']);
