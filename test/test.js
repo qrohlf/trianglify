bbeaeb5d276474e8cfc6b26834fed12ccff4061d
Quinn Rohlf <qr@qrohlf.com>
2015-02-06 12:06:59 -0800

    colorbrewer palettes & dependency updates

--- test/test.js
+++ test/test.js
@@ -12,18 +12,28 @@ describe('Trianglify', function(){
 
     it('should have default options', function() {
       var t = new Trianglify();
-      t.options.should.equal(Trianglify.defaults);
+      for (var key in t.defaults) {
+        t.options.should.have.property(key);
+      }
     });
 
     it('should override defaults with provided options', function() {
       var t = new Trianglify({cell_size: 1234, cell_padding: 567});
       t.options.cell_size.should.equal(1234);
       t.options.cell_padding.should.equal(567);
+      t.options.should.not.equal(Trianglify.defaults);
     });
 
     it('should throw an error on invalid options', function() {
       (function() {new Trianglify({bad_option: true});}).should.throw(Error);
     });
+
+    it('should give a random color from the colorbrewer palette if no colors are provided', function() {
+      var t = new Trianglify();
+      t.options.x_colors.should.have.length.within(3,11);
+      t.options.y_colors.should.have.length.within(3,11);
+    });
+
   });
 
 });
