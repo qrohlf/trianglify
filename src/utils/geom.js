56177ba787898b41bfc46a6ce3a8d748171156ac
Quinn Rohlf <qr@qrohlf.com>
2020-04-02 12:45:56 -0700

    working-ish 3d debugger

--- src/utils/geom.js
+++ src/utils/geom.js
@@ -10,19 +10,19 @@ export const getCentroid = d => {
   }
 }
 
-// given an array of coordinates representing a triangle in 3d space,
-// find the normal vector of the triangle, normalize it to a unit vector,
-// and return it as an [x, y, z] array
-export const getNormal = (poly) => {
-  const a = poly.vertices[0]
-  const b = poly.vertices[1]
-  const c = poly.vertices[2]
-  const ab = math.subtract(b, a)
-  const ac = math.subtract(c, a)
-  // get cross product
-  const cross = math.cross(ac, ab)
-  // normalize
-  const length = Math.sqrt(cross[0] * cross[0] + cross[1] * cross[1] + cross[2] * cross[2])
-  const norm = [cross[0] / length, cross[1] / length, cross[2] / length]
-  return norm
-}
+// // given an array of coordinates representing a triangle in 3d space,
+// // find the normal vector of the triangle, normalize it to a unit vector,
+// // and return it as an [x, y, z] array
+// export const getNormal = (poly) => {
+//   const a = poly.vertices[0]
+//   const b = poly.vertices[1]
+//   const c = poly.vertices[2]
+//   const ab = math.subtract(b, a)
+//   const ac = math.subtract(c, a)
+//   // get cross product
+//   const cross = math.cross(ac, ab)
+//   // normalize
+//   const length = Math.sqrt(cross[0] * cross[0] + cross[1] * cross[1] + cross[2] * cross[2])
+//   const norm = [cross[0] / length, cross[1] / length, cross[2] / length]
+//   return norm
+// }
