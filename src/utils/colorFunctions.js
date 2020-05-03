5070e9d4d9dc2b65895a1a5d9171ef0924e0a0a0
Quinn Rohlf <qr@qrohlf.com>
2020-05-03 08:35:24 -0700

    cleanup colorFunction method signatures

--- src/utils/colorFunctions.js
+++ src/utils/colorFunctions.js
@@ -13,11 +13,16 @@ import * as geom from './geom'
 // the snippet above gives you a trianglify pattern with a 20% random
 // jitter applied to the x and y gradient scales
 
+// Linear interpolation of two gradients, one for the x and one for the y
+// axis. This is the default Trianglify color function.
+// The bias parameter controls how prevalent the y axis is versus the x axis
 export const interpolateLinear = (bias = 0.5) => (
   ({xPercent, yPercent, xScale, yScale, opts}) =>
     chroma.mix(xScale(xPercent), yScale(yPercent), bias, opts.colorSpace)
 )
 
+// Give the pattern a 'sparkle' effect by introducing random noise into the
+// x and y gradients, making for higher contrast between cells.
 export const sparkle = (jitterFactor = 0.15) => (
   ({xPercent, yPercent, xScale, yScale, opts, random}) => {
     const jitter = () => (random() - 0.5) * jitterFactor
@@ -27,8 +32,11 @@ export const sparkle = (jitterFactor = 0.15) => (
   }
 )
 
+// This is similar to the sparkle effect, but instead of swapping colors around
+// it darkens cells by a random amount. The shadowIntensity parameter controls
+// how dark the darkest shadows are.
 export const shadows = (shadowIntensity = 0.8) => {
-  return ({vertexIndices, xPercent, yPercent, xScale, yScale, opts, points, random}) => {
+  return ({xPercent, yPercent, xScale, yScale, opts, random}) => {
     const a = xScale(xPercent)
     const b = yScale(yPercent)
     const color = chroma.mix(a, b, 0.5, opts.colorSpace)
