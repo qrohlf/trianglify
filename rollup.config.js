a7b102b8c4f3c91ef29b74149f0d2b64ede45239
Quinn Rohlf <qr@qrohlf.com>
2020-05-04 08:58:36 -0700

    transpile to es5 for older browsers/node runtimes

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
@@ -7,19 +8,20 @@ export default [
   { // build for node & module bundlers
     input: 'src/trianglify.js',
     external: ['chroma-js', 'delaunator', 'canvas'],
+    plugins: [babel()],
     output: { file: pkg.main, format: 'cjs' }
   },
   {
     // build minified bundle to be used standalone for browser use
     // note: // chroma.js weighs 40k minified, a smaller solution would be nice
     input: 'src/trianglify.js',
-    plugins: [terser({ output: { comments: false } }), resolve({ browser: true }), commonjs()],
+    plugins: [terser({ output: { comments: false } }), resolve({ browser: true }), commonjs(), babel()],
     output: { file: 'dist/trianglify.bundle.js', format: 'umd', name: 'trianglify' }
   },
   {
     // build non-minified bundle to be used for debugging
     input: 'src/trianglify.js',
-    plugins: [resolve({ browser: true }), commonjs()],
+    plugins: [resolve({ browser: true }), commonjs(), babel()],
     output: { file: 'dist/trianglify.bundle.debug.js', format: 'umd', name: 'trianglify' }
   }
 ]
