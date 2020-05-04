91409a9e3fd786e4f7ecbb9509a5da84166c4102
Quinn Rohlf <qr@qrohlf.com>
2020-05-04 09:16:46 -0700

    theoretically, this should work in all target environments

--- rollup.config.js
+++ rollup.config.js
@@ -8,20 +8,20 @@ export default [
   { // build for node & module bundlers
     input: 'src/trianglify.js',
     external: ['chroma-js', 'delaunator', 'canvas'],
-    plugins: [babel()],
+    plugins: [babel({ babelHelpers: 'bundled' })],
     output: { file: pkg.main, format: 'cjs' }
   },
   {
     // build minified bundle to be used standalone for browser use
     // note: // chroma.js weighs 40k minified, a smaller solution would be nice
     input: 'src/trianglify.js',
-    plugins: [terser({ output: { comments: false } }), resolve({ browser: true }), commonjs(), babel()],
+    plugins: [terser({ output: { comments: false } }), resolve({ browser: true }), commonjs(), babel({ babelHelpers: 'bundled' })],
     output: { file: 'dist/trianglify.bundle.js', format: 'umd', name: 'trianglify' }
   },
   {
     // build non-minified bundle to be used for debugging
     input: 'src/trianglify.js',
-    plugins: [resolve({ browser: true }), commonjs(), babel()],
+    plugins: [resolve({ browser: true }), commonjs(), babel({ babelHelpers: 'bundled' })],
     output: { file: 'dist/trianglify.bundle.debug.js', format: 'umd', name: 'trianglify' }
   }
 ]
