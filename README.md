0e80b1d3f9a2dfa90e0cad06de6bb77e9ebdd6db
Quinn Rohlf <qr@qrohlf.com>
2020-05-08 15:33:44 -0700

    gh pages next

---
+++ README.md
@@ -0,0 +1,34 @@
+# gh-pages
+
+Empty project.
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To run in hot module reloading mode:
+
+```sh
+npm start
+```
+
+To create a production build:
+
+```sh
+npm run build-prod
+```
+
+## Running
+
+```sh
+node dist/bundle.js
+```
+
+## Credits
+
+Made with [createapp.dev](https://createapp.dev/)
+
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import trianglify from 'trianglify'
+
+
+const target = document.getElementById('pattern')
+
+const cycle = (overrides = {}) => trianglify({
+  height: window.innerWidth * 0.25,
+  width: window.innerWidth * 0.25,
+  variance: Math.random() > 0.7 ? 0 : 1,
+  ...overrides
+}).toCanvas(target)
+
+// force RdBu for the first cycle
+cycle({xColors: trianglify.utils.colorbrewer.RdBu})
+setInterval(cycle, 5000)
