69e0ec729db1bd0ce28bd0ee4321af405de4af3c
Quinn Rohlf <qr@qrohlf.com>
2020-05-12 08:49:36 -0700

    mobile styling

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import trianglify from 'trianglify'
 const target = document.getElementById('pattern')
 
 const cycle = (overrides = {}) => trianglify({
+  cellSize: window.innerWidth * 0.25 / 7,
   height: window.innerWidth * 0.25,
   width: window.innerWidth * 0.25,
   variance: Math.random() > 0.7 ? 0 : 1,
